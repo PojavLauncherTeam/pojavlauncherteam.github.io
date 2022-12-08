184591074efddc28054a320a577357f3bf05779e
CriticalRange <muhammedkosgen0608112@gmail.com>
2022-12-09 00:30:02 +0300

    Add LAYOUT-INTRO.MD to config.js
    While doing that, I hope i also fixed the **BUILD-FROM-SOURCE**'s indentation :)
    
    Signed-off-by: CriticalRange <muhammedkosgen0608112@gmail.com>

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -85,7 +85,8 @@ module.exports = {
 					sidebarDepth: 1,
 					children: [
 					    'INSTALL', // Installing PojavLauncher
-                        'BUILD-FROM-SOURCE', // Building PojavLauncher from source
+                        			'BUILD-FROM-SOURCE', // Building PojavLauncher from source
+						'LAYOUT-INTRO', // Layout Introduction for PojavLauncher
 						'SIGN-IN', // Signing in
 						'INSTALL-MC', // Downloading Minecraft
 						'DEFAULT-CONTROLS', // Taking a look at the controls
