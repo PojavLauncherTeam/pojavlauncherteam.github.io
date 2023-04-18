61d04ae7b2d261efaf301030c955539626c9b833
uppedplays@gmail.com <uppedplays@gmail.com>
2023-04-18 20:07:50 +0800

    chore: mix theme pre...uhh (1)

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+import { defineUserConfig } from 'vuepress'
+import mixTheme from 'vuepress-theme-mix'
+
+export default defineUserConfig({
+  // ...
+
+  theme: mixTheme({
+    // configure the theme here
+  }),
+
+  // ...
+})
