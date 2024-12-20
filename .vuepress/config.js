91ea84f75892bd57ccd4d96a47c8f1ed6afc2d45
Dev1ss0 <94245667+Dev1ss0@users.noreply.github.com>
2024-12-20 17:42:56 +0100

    Update config.js
    Signed-off-by: Dev1ss0 <94245667+Dev1ss0@users.noreply.github.com>

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -80,47 +80,47 @@ module.exports = {
             
             children: [
 				{
-					title: 'Getting Started', // Subcategory for starting off
-					collapsable: false, // Don't allow collapsing
+					title: 'Getting Started', // Subcategory for starting off.
+					collapsable: false, // Don't allow collapsing.
 					sidebarDepth: 1,
 					children: [
-					    'INSTALL', // Installing PojavLauncher
-                        'BUILD-FROM-SOURCE', // Building PojavLauncher from source
-						'SIGN-IN', // Signing in
-						'INSTALL-MC', // Downloading Minecraft
-						'CUSTOM-CONTROLS', // Customizing the on-screen controls
-						'DISCORD', // Official PojavLauncher Discord server
+					    'INSTALL', // Installing PojavLauncher.
+                        'BUILD-FROM-SOURCE', // Building PojavLauncher from source.
+						'SIGN-IN', // Signing in.
+						'INSTALL-MC', // Downloading Minecraft.
+						'CUSTOM-CONTROLS', // Customizing the on-screen controls.
+						'SOCIALMEDIA', // Official PojavLauncher social medias.
 					]
 				},
 				{
-					title: 'Frequently Asked Questions', // Subcategory for FAQ
+					title: 'Frequently Asked Questions', // Subcategory for FAQ.
 					collapsable: true, 
 					sidebarDepth: 1,
 					children: [
 						{
-							title: 'iOS-specific', // Subcategory for iOS FAQ
-							collapsable: true, // Don't allow collapsing
+							title: 'iOS-specific', // Subcategory for iOS FAQ.
+							collapsable: true, // Don't allow collapsing.
 							sidebarDepth: 1,
 							children: [
-								'JIT', // iOS and "JIT"
+								'JIT', // iOS and "JIT".
 								'APPSTORE', // Will PojavLauncher ever come to App Store for iOS?
 							]
 						},
 						{
-							title: 'Android-specific', // Subcategory for Android FAQ
-							collapsable: true, // Don't allow collapsing
+							title: 'Android-specific', // Subcategory for Android FAQ.
+							collapsable: true, // Don't allow collapsing.
 							sidebarDepth: 1,
 							children: [
 								'PARSINGERROR', // "Error while parsing the package" error fix.
-								'REFUSALOFINSTALL', // "App not installed" error fix. (very uncommon)
-								'CANTFINDLOG', // Can't find the log file. (non-root)
-								'CANTFINDMINECRAFT', // Can't find .minecraft (non-root)
+								'REFUSALOFINSTALL', // "App not installed" error fix (very uncommon).
+								'CANTFINDLOG', // Can't find the log file (non-root).
+								'CANTFINDMINECRAFT', // Can't find .minecraft (non-root).
 								'ZINKNOTWORKING', // Zink doesn't work.
 								'MODCRASHING', // Installing any mod results in a crash.
 								'MODNOTSHOWINGUP', // My mods don't show up in-game.
 								'JAVARUNTIMES', // Installing Java runtimes manually.
 								'JARCRASHED', // Crash when executing a jar.
-								'32BIT', // Platform and performance limitations. (32bit)
+								'32BIT', // Platform and performance limitations (32bit).
 								'FFMPEG', // Setting up FFMPEG, for Replay Mod.
 								'SHADERS', // Look what we got here.
 								'RENDERERS', // I have no idea what they do and why PojavLauncher needs it.
@@ -136,16 +136,16 @@ module.exports = {
 		
 				},
 				{
-					title: 'Going further', // Subcategory for getting deeper in the launcher
+					title: 'Going further', // Subcategory for getting deeper in the launcher.
 					collapsable: true, 
 					sidebarDepth: 1,
 					children: [
-						'OPTIMIZATION', // Optimizing the game
+						'OPTIMIZATION', // Optimizing the game.
 						'CUSTOM-MC', // Custom Minecraft!
-						'MODPACKS', // Everything you need to know about Modpacks support
-						'PROBLEMATIC-MODS-COMMON-FIXES', // List of available fixes for popular modpacks
-						'ENV-VARS', // Using environment variables
-						'JAVA-ARGS', // Adding custom Java arguments
+						'MODPACKS', // Everything you need to know about Modpacks support.
+						'PROBLEMATIC-MODS-COMMON-FIXES', // List of available fixes for popular modpacks.
+						'ENV-VARS', // Using environment variables.
+						'JAVA-ARGS', // Adding custom Java arguments.
 					]
 		
 				},
@@ -154,35 +154,35 @@ module.exports = {
 
         },
         {
-            title: 'About PojavLauncher', // Category about the project
+            title: 'About PojavLauncher', // Category about the project.
             collapsable: false,
             sidebarDepth: 1,
             children: [
-				'LEGAL', // Legal + Security
-				'DEVICES', // Supported Devices
-				'MCVERS', // Supported Minecraft Versions
-				'MODS-UNSUPPORTED', // Unsupported Mods
+				'LEGAL', // Legal + Security.
+				'DEVICES', // Supported Devices.
+				'MCVERS', // Supported Minecraft Versions.
+				'MODS-UNSUPPORTED', // Unsupported Mods.
             ]
 
         },
 		{
-            title: 'Contributing', // Category about contributing
+            title: 'Contributing', // Category about contributing.
             collapsable: false,
             sidebarDepth: 1,
             children: [
-				'CONT-WEBSITE', // Contributing to this website
-               			'CONT-TRANSLATIONS', //Contributing to PojavLauncher translations, hopefully website translations later on
+				'CONT-WEBSITE', // Contributing to this website.
+               			'CONT-TRANSLATIONS', //Contributing to PojavLauncher translations, hopefully website translations later on.
             ]
 
         },
         {
-            title: 'Recent Updates', // Category about recent developments
+            title: 'Recent Updates', // Category about recent developments.
             collapsable: false,
             sidebarDepth: 1,
             children: [
-				'UNJAIL', // Bringing PojavLauncher to unjailbroken iOS
-				'IOS-SUPPORT', // System support in future versions of PojavLauncher iOS
-				'LOCAL-MODE', // Offline mode is going places
+				'UNJAIL', // Bringing PojavLauncher to unjailbroken iOS.
+				'IOS-SUPPORT', // System support in future versions of PojavLauncher iOS.
+				'LOCAL-MODE', // Offline mode is going places.
             ]
 
         },
